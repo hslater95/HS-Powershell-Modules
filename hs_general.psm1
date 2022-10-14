a6b2d5f830ef6a74b0d022b3401cd3f24745dbf7
hslater95 <107321650+hslater95@users.noreply.github.com>
2022-10-13 23:31:27 -0700

    Update hs_general.psm1

--- hs_general.psm1
+++ hs_general.psm1
@@ -1,64 +1,9 @@
 Clear-Host
-set-location $HOME
-new-alias -name c -value "C:\Users\hslater\AppData\Local\Programs\Microsoft VS Code\bin\code.cmd"
-new-alias -name anki -value "C:\users\hslater\anki.lnk"
-new-alias -name slack -value c:\users\hslater\slack.lnk
-set-alias -name p -value powershell
+Set-Location $home
 set-executionpolicy -executionpolicy unrestricted -force
-function prof { set-location "c:\windows\system32\windowspowershell\v1.0"}
-function mods {set-location "C:\WINDOWS\system32\WindowsPowerShell\v1.0\Modules"}
-function contains_improper_characters($str) {
-function profile_path {return " C:\windows\system32\windowspowershell\v1.0\profile.ps1"}    
-
-    if ($str -like "*'*") {
-        $x = ($str -like "*'*")
-        return $x 
-    }
-    $improper_chars = [regex]::new('["!@#\$%\^&\*()\+=`~\?<>\.]')
-    $match = $improper_chars.match($str)
-    return $match.Success
-}
-
-function add_hs($str) {
-        $new_str = $null;
-        
-
-
-       
-    if ($str -match '^\s*_+') {
-        $new_str = $str -replace '^\s*_+', 'hs_'
-        $new_str = remove_whitespace($new_str)
-       return $new_str 
-    }
-    if ($str -match '^hs_') {
-       $new_str = $str -replace '^hs_', 'hs_'
-       $new_str = remove_whitespace($new_str)
-       return $new_str 
-    }
-    if ($str -match '^\s*hs_') {
-        $new_str = $str -replace '^\s*hs_', 'hs_'
-        $new_str = remove_whitespace($new_str)
-       return $new_str   
-    }
-    if ($str -match 'hs\w') {
-        $new_str = $str -replace 'hs', 'hs_'
-        $new_str = remove_whitespace($new_str)
-       return $new_str 
-    }
-    if ($str -notmatch '^hs_') {
-        $new_str = $str.trimstart()
-        $new_str = "hs_" + $new_str
-        $new_str = remove_whitespace($new_str)
-       return $new_str 
-    }
-    
-    
-}
-
-function remove_whitespace($str) {
-    $new_str = $null
-    $new_str = $str.trim()
-    $new_str = $new_str -replace '\s+', '_'
-    return $new_str  
-
-}
+Set-Alias -Name p -Value powershell
+Set-Alias -Name c -Value code
+Set-Alias -Name n -Value node
+$global:mods = "C:\Program Files\windowspowershell\modules"
+#profile == $profile
+#home == $home
